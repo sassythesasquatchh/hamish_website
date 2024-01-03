2690c407fca4ac7331e13b140bae82ad389c2e54
sassythesasquatchh <pcd276@nyu.edu>
2024-01-03 21:54:27 +1300

    increased hero worship

--- src/App.js
+++ src/App.js
@@ -12,18 +12,25 @@ const buttonStyles = {
   transition: "background-color 0.3s ease",
 };
 
+const patrickText =
+  'Every single day, many, many people think they can claim that their brother is the "best brother in the world. These people, while undoubtably noble in their efforts, are all wrong. ';
+
 function App() {
+  function handleClick() {
+    // Replace 'https://example.com' with the URL of the webpage you want to open
+    window.location.href =
+      "https://www.stuff.co.nz/national/education/90180367/hamilton-student-top-in-the-world-for-sciences";
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          I love bo and i love him cause he is the best in every way shape and
-          form lol
-        </p>
+        <p>{patrickText}</p>
         <button className="square">I Love Bo</button>
         <button className="square">Patrick is vey quail</button>
         <img src={coolymccoolface} alt="Patrick Dowd" />
-        <button style={buttonStyles}> New Button </button>
+        <button style={buttonStyles} onClick={handleClick}>
+          Proof
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
