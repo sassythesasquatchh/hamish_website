77ebaf4bc14f150b612a49072bb36be4aafa3de7
sassythesasquatchh <pcd276@nyu.edu>
2024-01-03 21:34:46 +1300

    initial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import coolymccoolface from "./patrick.jpg";
+
+const buttonStyles = {
+  padding: "10px 100px",
+  borderRadius: "10px",
+  backgroundColor: "red",
+  color: "FF69B4",
+  border: "none",
+  cursor: "",
+  fontSize: "32px",
+  transition: "background-color 0.3s ease",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I love bo and i love him cause he is the best in every way shape and
+          form lol
         </p>
+        <button className="square">I Love Bo</button>
+        <button className="square">Patrick is vey quail</button>
+        <img src={coolymccoolface} alt="Patrick Dowd" />
+        <button style={buttonStyles}> New Button </button>
         <a
           className="App-link"
           href="https://reactjs.org"
